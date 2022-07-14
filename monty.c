e1c5d45d979b1b91b4fe9b202feede53c6a761a1
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-07-14 22:47:43 +0100

    update

--- monty.c
+++ monty.c
@@ -6,7 +6,7 @@
  * @argv: argument variables
  * Return: EXIT_SUCCESS
  */
-int main(int argc, __attribute__((unused)) char **argv)
+int main(int argc, char **argv)
 {
 	int montyfile, readfile;
 	char *buffer;
